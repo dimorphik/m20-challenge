834b2c24ac551ff2090145a57b75a0bf5c1bb99e
Robin <45707086+dimorphik@users.noreply.github.com>
2023-06-10 23:59:09 -0700

    overriding bootstrap colors

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const onDocumentReady = () => {
+  console.log("hello there!");
+  /*
+  $(".nav-item.active").click((e) => {
+    e.preventDefault();
+  });
+*/
+};
+
+$(document).ready(onDocumentReady);
