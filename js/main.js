43a4b3d2305442bb64889762460a4bf195da39ae
Robin <45707086+dimorphik@users.noreply.github.com>
2023-06-12 02:54:57 -0700

    comments & adjustments

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+/* 
+  This chunk of jQuery code makes a subtle change to the directional icons
+  of the Bootstrap carousel:
+
+  When the user hovers over either the "left" or "right" arrow icon of a 
+  carousel, this code will change the icon outline to a "filled" version.
+*/
 const onDocumentReady = () => {
   $(".bi-caret-left").hover(function () {
     $(this).toggleClass("bi-caret-left bi-caret-left-fill");
