d215b07dab690fb6e9dbb27fe523c74484f0e7e9
Robin <45707086+dimorphik@users.noreply.github.com>
2023-06-12 01:15:34 -0700

    products page responsive strategy

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 const onDocumentReady = () => {
-  console.log("hello there!");
-  /*
-  $(".nav-item.active").click((e) => {
-    e.preventDefault();
+  $(".bi-caret-left").hover(function () {
+    $(this).toggleClass("bi-caret-left bi-caret-left-fill");
   });
-*/
+
+  $(".bi-caret-right").hover(function () {
+    $(this).toggleClass("bi-caret-right bi-caret-right-fill");
+  });
 };
 
 $(document).ready(onDocumentReady);
